c0d13102e8f6b14d855730eabbccce32d2c8763f
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-10-18 17:19:46 -0700

    table

--- practice/practice.js
+++ practice/practice.js
@@ -1,9 +1,9 @@
 
 let listTasks = [];
 //constructor
-function Task(descrition, priority){
-    this.title= descrition;//this is the problematic***
-    this.priority=priority;
+function Task(descrition, priority) {
+    this.title = descrition;//this is the problematic***
+    this.priority = priority;
 
 
 }
@@ -11,20 +11,21 @@ function Task(descrition, priority){
 
 //globlal varible 
 let inputPractice = document.getElementById("txtPractice");//getting the html 
-let selectPriority =document.getElementById("selPriority");
+let selectPriority = document.getElementById("selPriority");
 let task = "";
 
-function register(){
-        let newTask = new Task(inputPractice.value,selectPriority.value);
-        listTasks.push(newTask);
-        display(newTask);
-        display();
-        inputPractice.value="";//clear the input 
-        inputPractice.className="error"//adding the css class 
-    
+function register() {
+    let newTask = new Task(inputPractice.value, selectPriority.value);
+    listTasks.push(newTask);
+    display(newTask);
+    display();
+    inputPractice.value = "";//clear the input 
+    inputPractice.className = "error"//adding the css class 
+
 }
-function display(aTask){
+function display(aTask) {
     let taskSection = document.getElementById('tasks')//getting the HTML element
-    task+= `<li class="task">${aTask.title} --- ${aTask.priority} <button>🧺</button></li>`;//create the task HTML element
-    taskSection.innerHTML=task;//injection the task into the HTML 
+    //create the task HTML element
+    task += `<tr><td class="title">${aTask.title}</td> <td class="priority">${aTask.priority}</td></tr>`
+    taskSection.innerHTML = task;//injection the task into the HTML 
 }
