46f8de9952185a78c2dd973178512789607784b0
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-10-20 18:21:37 -0700

    readUser

--- javaScript/storeManager.js
+++ javaScript/storeManager.js
@@ -1,9 +1,10 @@
 function saveUser(newUser) {
     console.log(newUser);
-    let val = JSON.stringify(newUser);
+    let val = JSON.stringify(newUser);//convert the object to a string
     console.log(val);
     localStorage.setItem("user", val);
 }
 function readUser(){
-    
+    let users = localStorage.getItem("user");//getting the user from local storage
+    console.log(users);
 }
